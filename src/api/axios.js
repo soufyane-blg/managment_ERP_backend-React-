bfb5902e5d8f5aa07987d3e9e544580d49c87978
soufyane <soufyanee.blg@gmail.com>
2026-04-25 22:15:37 +0300

    fix urls

--- src/api/axios.js
+++ src/api/axios.js
@@ -12,7 +12,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
 
-  if (token && !config.url.includes("/auth/token")) {
+  if (token && !config.url.includes("/auth/token/")) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
